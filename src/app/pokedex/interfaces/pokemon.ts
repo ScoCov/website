7cbe49419ce1c25cc06cd2bf8b726b7f65d10361
ScoCov <153954098+ScoCov@users.noreply.github.com>
2023-12-27 13:55:38 -0700

    modified:   src/app/pokedex/interfaces/pokemon.ts 	modified:   src/app/pokedex/pokemondetail/pokemondetail.component.css 	modified:   src/app/pokedex/pokemondetail/pokemondetail.component.html

--- src/app/pokedex/interfaces/pokemon.ts
+++ src/app/pokedex/interfaces/pokemon.ts
@@ -19,8 +19,96 @@ export interface PokemonDetails extends Pokemon{
   past_abilities?: [],
   past_types?: [],
   species?: [],
-  sprites?: [],
+  sprites?: Sprites,
   stats?: [],
   types?: [],
   weight?: number
+}
+
+interface Sprites extends Sprites_Front, Sprites_Back {
+  other: Sprites_Other,
+  versions: Sprites_Versions
+}
+
+interface Sprites_Front {
+  front_default: string,
+  front_female: string,
+}
+
+interface Sprites_Front_Shiny {
+  front_shiny: string,
+  front_female_shiny: string,
+}
+
+interface Sprites_Back {
+  back_default: string,
+  back_female: string,
+}
+
+interface Sprites_Back_Shiny {
+  back_default_shiny: string,
+  back_female_shiny: string,
+}
+
+
+interface Sprites_Other {
+  dream_world: Sprites_Front,
+  home: Sprites_Other_Home
+  official_artwork: Sprites_Front,
+  showdown: Sprites_Other_Showdown
+}
+
+interface Sprites_Versions {
+  generation_i: Sprites_Generation_I,
+  generation_ii: Sprites_Generation_II,
+}
+
+interface Sprites_Other_Home extends Sprites_Front, Sprites_Front_Shiny{}
+
+interface Sprites_Other_Showdown extends Sprites_Front, Sprites_Front_Shiny, Sprites_Back, Sprites_Back_Shiny{}
+
+interface Sprites_Generation_I {
+  red_blue: {
+    back_default: string,
+    back_gray: string,
+    back_transparent: string,
+    front_default: string,
+    front_gray: string,
+    front_transparent: string,
+  }
+  yellow: {
+    back_default: string,
+    back_gray: string,
+    back_transparent: string,
+    front_default: string,
+    front_gray: string,
+    front_transparent: string,
+  }
+}
+
+interface Sprites_Generation_II {
+  crystal: {
+    back_default: string,
+    back_shiny: string,
+    back_transparent: string,
+    back_shiny_transparent: string,
+    front_default: string,
+    front_shiny: string,
+    front_transparent: string,
+    front_shiny_transparent: string
+  },
+  gold: {
+    back_default: string,
+    back_shiny: string,
+    front_default: string,
+    front_shiny: string,
+    front_transparent: string,
+  },
+  silver: {
+    back_default: string,
+    back_shiny: string,
+    front_default: string,
+    front_shiny: string,
+    front_transparent: string,
+  }
 }
