0900c4b90b713104ec77a7e3d08b2745be19d0a0
ScoCov <153954098+ScoCov@users.noreply.github.com>
2025-02-19 20:56:14 -0700

    Renewed Interest

--- src/app/resume/data/workexpiernce.ts
+++ src/app/resume/data/workexpiernce.ts
@@ -2,89 +2,54 @@ import { Workexperience } from "../interfaces/workexperience";
 
 export const WORK_EXPERIENCE: Workexperience[] = [
   {
+    company_name: 'Pima Count Superior Court',
+    company_location: 'Tucson, Az',
+    start_date: '',
+    end_date: '',
+    job_title: 'Software Engineer I',
+    duties: [
+
+    ]
+  },
+  {
     company_name: 'Mindplay Education',
     company_location: 'Tucson, Az',
-    start_date: 'May 2022',
-    end_date: 'September 2023',
+    start_date: '',
+    end_date: '',
     job_title: 'Software Developer',
     duties: [
-      'Provided support to maintain legacy code, multiple languages and frameworks. Primarily C#, for our backend, and JavaScript/Typescript specific frameworks for our frontend.',
-      'Collaborated with other developers using Agile methodologies.',
-      'Coordinate with the artists to obtain new images for the product.',
-      'Worked with educators to make sure product fit education standards. ',
-      'Created responsive UIs utilizing the Angular Framework',
-      'Develop strategies to improve efficiency of developing parts of the product.',
-      'Migrated the database to the cloud, also changing its’ data base language.',
-      'Worked independently of other developers, but would Collaborate with others to accomplish product owner’s requests when needed.',
+
     ]
   },
   {
     company_name: 'City of Tucson',
     company_location: 'Tucson, Az',
-    start_date: 'September 2021',
-    end_date: 'Janurary 2022',
-    job_title: 'Quality Assurance and IT Training Intern',
+    start_date: '',
+    end_date: '',
+    job_title: 'QA and IT Intern',
     duties: [
-      'Assisted in projects by conducting meetings and providing technical expertise. ',
-      'Collaborated with my team and developed the first monthly lessons related to software for the city employees due to increased remote workers. ',
-      'Conducted meetings manys employees spanning multiple departments to collect data for new upcoming software distributed in their departments',
-      'Developed spreadsheets to record the status of the meetings, the results, and if any further contact was made. ',
-    ]
-  },
-  {
-    company_name: 'Mr. Janitor',
-    company_location: 'Tucson, Az',
-    start_date: 'Janurary 2020',
-    end_date: 'September 2021',
-    job_title: 'Janitor',
-    duties: [
-      'Ensured all surfaces were cleaned and sanitized per company standards.',
-      'Restocked supplies in multiple locations to facilitate an effective next business day.',
-      'Coordinated monthly stock orders with supervisors regarding supplies.',
-      'Collaborated with others to clean extensive facilities for multiple locations each night.',
+
     ]
   },
   {
     company_name: 'SWS Computers',
     company_location: 'Tucson, Az',
-    start_date: 'July 2018',
-    end_date: 'September 2019',
+    start_date: '',
+    end_date: '',
     job_title: 'Customer Service/ Recycler',
     duties: [
-      'Provided information for customers’ computer-related projects and questions. ',
-      'Troubleshot mild computer issues for clients.',
-      'Recorded customer information for servicing their computers when the issues could not be solved quickly.',
-      'Established a thriving clearance section to sell refurbished PCs and electronic devices from recycled devices from clients. ',
-      'Dissembled electronics that customers donated',
+
     ]
   },
   {
     company_name: 'Geeks2You',
     company_location: 'Tucson, Az',
-    start_date: 'Janurary 2018',
-    end_date: 'July 2018',
+    start_date: '',
+    end_date: '',
     job_title: 'Mobile Computer Techincian',
     duties: [
-      'Remove viruses and malware and improve the performance of the client’s PC; educate the client on proper safety policies when navigating the web and using their PCs.',
-      'Installed printers/scanners with static IP to prevent issues of printers dropping from the client’s PC.',
-      'Taught customers how to build a computer by walking them through the process.',
-      'Created euphemisms and metaphors to communicate tech concepts to those unknowledgeable about technology to ensure the customer could understand.',
-      'Recorded Customer Information, if new, added it to the database, and detailed the service in our service forms.',
+
     ]
-  },
-  {
-    company_name: 'UPS Store',
-    company_location: 'Tucson, Az',
-    start_date: 'August 2013',
-    end_date: 'Junarauary 2018',
-    job_title: 'Customer Service Representative',
-    duties: [
-      'Input customer information into the system’s database for shipping.',
-      'Recorded the weight and dimensions of packages.',
-      'Packaged customers’ items according to insurance standards.',
-      'Restocked shelves of office supplies and shipping supplies.',
-      'Used proprietary software to prepare shipping labels and payments and create or update customer Accounts for shipping and mailbox services.',
-      'Separated and sorted mail into mailboxes.',
-    ]
-  },
+  }
+
 ]
