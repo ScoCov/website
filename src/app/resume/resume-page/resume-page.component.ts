06c323a79ebebae575901c5a892cf023c8e21716
ScoCov <153954098+ScoCov@users.noreply.github.com>
2025-02-26 18:21:33 -0700

    Including Education on the Resume

--- src/app/resume/resume-page/resume-page.component.ts
+++ src/app/resume/resume-page/resume-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ResumeService } from '../resume.service';
 import { Workexperience } from '../interfaces/workexperience';
 import { Skill } from '../interfaces/skill';
+import { Education } from '../interfaces/education';
 
 @Component({
   selector: 'app-resume-page',
@@ -11,12 +12,14 @@ import { Skill } from '../interfaces/skill';
 export class ResumePageComponent implements OnInit {
   public work_experience!: Workexperience[];
   public skills!: Skill[];
+  public education!: Education;
 
   constructor(private resumeService: ResumeService) { }
 
   ngOnInit(): void {
     this.work_experience = this.resumeService.getWorkExperience();
     this.skills = this.resumeService.getSkills();
+    this.education = this.resumeService.getEducation();
   }
 
 }
--- src/app/resume/resume.service.ts
+++ src/app/resume/resume.service.ts
@@ -1,6 +1,10 @@
+import { EDUCATION } from './data/education';
 import { Injectable } from '@angular/core';
 import { WORK_EXPERIENCE } from './data/workexpiernce';
 import { SKILL_SET } from './data/skills';
+import { Education } from './interfaces/education';
+import { Skill } from './interfaces/skill';
+import { Workexperience } from './interfaces/workexperience';
 
 @Injectable({
   providedIn: 'root'
@@ -9,11 +13,15 @@ export class ResumeService {
 
   constructor() { }
 
-  getWorkExperience(){
+  getWorkExperience(): Workexperience[]{
     return WORK_EXPERIENCE;
   }
 
-  getSkills() {
+  getSkills(): Skill[] {
     return SKILL_SET;
+  }
+
+  getEducation(): Education {
+    return EDUCATION;
   }
 }
